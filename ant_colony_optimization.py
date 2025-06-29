4b902a460102501a686c22cb079d527c3c623797
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2025-06-29 16:55:13 -0300

    Add logic for ant position update

--- ant_colony_optimization.py
+++ ant_colony_optimization.py
@@ -17,7 +17,6 @@ class Ant:
         :param upper_bound: upper bound of the ant position.
         :type upper_bound: numpy array.
         """
-        # Todo
         self.dimension = np.size(lower_bound)
         self.position = np.zeros(self.dimension)
 
@@ -35,11 +34,9 @@ class AntColonyOptimization:
     Represents the Ant Colony Optimization algorithm.
     Hyperparameters:
         num_ants: number of ants.
-        evaporation_rate: evaporation rate.
-        pheromone_intensity: pheromone intensity.
-        alpha: pheromone influence.
-        beta: heuristic influence.
         num_best_solutions: number of solutions used to build Gaussian Mixture
+        evaporation_rate: evaporation rate (xi), controls the search shrinkage
+        q: selection parameter that controls the selection pressure
 
     :param hyperparams: hyperparameters used by Ant Colony Optimization.
     :type hyperparams: Params.
@@ -56,9 +53,7 @@ class AntColonyOptimization:
         self.num_ants = hyperparams.num_ants
         self.num_best_solutions = hyperparams.num_best_solutions
         self.evaporation_rate = hyperparams.evaporation_rate
-        self.pheromone_intensity = hyperparams.pheromone_intensity
-        self.alpha = hyperparams.alpha
-        self.beta = hyperparams.beta
+        self.q = hyperparams.q
 
         self.ants = [
             Ant(self.lower_bound, self.upper_bound) for _ in range(self.num_ants)
@@ -78,6 +73,8 @@ class AntColonyOptimization:
         :return: the best position.
         :rtype: numpy array.
         """
+        if np.all(self.best_rewards == -inf):
+            return None
         return self.best_positions[np.argmax(self.best_rewards)]
 
     def get_best_value(self):
@@ -98,32 +95,75 @@ class AntColonyOptimization:
         """
         return self.ants[self.current_ant].position
 
+    def _update_ant_positions(self):
+        """
+        Main logic of ACO_R: Updates the position of all ants for the next generation.
+        """
+        k = self.num_best_solutions
+
+        valid_solutions_count = np.sum(self.best_rewards > -inf)
+        if valid_solutions_count < 2:
+            return
+
+        weights = (1 / (self.q * k * np.sqrt(2 * np.pi))) * np.exp(
+            -0.5 * (np.arange(k) ** 2) / (self.q**2 * k**2)
+        )
+        probabilities = weights / np.sum(weights)
+
+        for ant in self.ants:
+            selected_index = np.random.choice(k, p=probabilities)
+            guide_solution = self.best_positions[selected_index]
+
+            new_position = np.zeros(self.dimension)
+            for dim in range(self.dimension):
+                mu = guide_solution[dim]
+
+                sigma = (
+                    self.evaporation_rate
+                    * np.sum(np.abs(self.best_positions[:, dim] - mu))
+                    / (k - 1 + 1e-9)
+                )
+
+                new_position[dim] = random.gauss(mu, sigma)
+
+            ant.position = np.clip(new_position, self.lower_bound, self.upper_bound)
+            ant.path.append(ant.position)
+
     def advance_generation(self):
         """
         Advances the generation of Ants. Auxiliary method to be used by notify_evaluation().
         """
-        k = self.num_best_solutions
-        indexes = np.argsort(self.rewards_current_generation)[::-1]
-        sorted_generation_rewards = np.sort(self.rewards_current_generation)
-        sorted_generation_positions = self.positions_current_generation[indexes]
+        current_pos_arr = np.array(self.positions_current_generation)
+        current_rew_arr = np.array(self.rewards_current_generation)
 
-        merged_best_rewards = self.best_rewards + sorted_generation_rewards[:k]
-        merged_best_positions = self.best_positions + sorted_generation_positions[:k]
+        valid_best_indices = np.where(self.best_rewards > -inf)[0]
 
-        indexes = np.argsort(merged_best_positions)[::-1]
+        if valid_best_indices.size > 0:
+            combined_positions = np.vstack(
+                (self.best_positions[valid_best_indices], current_pos_arr)
+            )
+            combined_rewards = np.hstack(
+                (self.best_rewards[valid_best_indices], current_rew_arr)
+            )
+        else:
+            combined_positions = current_pos_arr
+            combined_rewards = current_rew_arr
 
-        sorted_merged_best_rewards = np.sort(merged_best_rewards)
-        sorted_merged_best_positions = self.merged_best_positions[indexes]
+        sorted_indices = np.argsort(combined_rewards)[::-1]
 
-        self.best_rewards = sorted_merged_best_rewards[:k]
-        self.best_positions = sorted_merged_best_positions[:k]
+        num_to_keep = min(self.num_best_solutions, len(sorted_indices))
+        top_indices = sorted_indices[:num_to_keep]
+
+        self.best_rewards = np.full(self.num_best_solutions, -inf)
+        self.best_positions = np.zeros((self.num_best_solutions, self.dimension))
+
+        self.best_rewards[:num_to_keep] = combined_rewards[top_indices]
+        self.best_positions[:num_to_keep] = combined_positions[top_indices]
 
         self.positions_current_generation = []
         self.rewards_current_generation = []
 
-        for ant in self.ants:
-            # todo: update ant position
-            pass
+        self._update_ant_positions()
 
     def notify_evaluation(self, value):
         """
@@ -135,6 +175,7 @@ class AntColonyOptimization:
         ant = self.ants[self.current_ant]
         self.positions_current_generation.append(ant.position)
         self.rewards_current_generation.append(value)
+        ant.reward = value
 
         self.current_ant += 1
         if self.current_ant == self.num_ants:
